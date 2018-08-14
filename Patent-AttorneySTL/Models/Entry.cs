8cfa4b0b85d1fea1cac29d9b8a62052ffb10535c
drewb <18552508+drewb101@users.noreply.github.com>
2018-08-13 20:03:24 -0500

    Added Models folder, entry class, & database excel sheet

---
+++ Patent-AttorneySTL/Models/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Patent_AttorneySTL.Models
+{
+    public class Entry
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleInitial { get; set; }
+        public string Company { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public int Phone { get; set; }
+        public string Status { get; set; }
+    }
+}
